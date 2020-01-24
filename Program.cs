0480e7325a3bf1b26735db0c2e6c5631a7c3bb99
Altessa <sikachev.o@gmail.com>
2020-01-24 13:11:52 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace polidrom
+{
+    public static class Extension
+    {
+        public static bool BoolPolindrom(this string text)
+        {
+            string str = Regex.Replace(text, "[ .,!?]", "");
+            char[] tempstr = str.ToCharArray();
+            Array.Reverse(tempstr);
+            string reverse = new string(tempstr); 
+            bool pol = true;
+            if (str.Equals(reverse))
+            {
+                return pol;
+            }
+            else
+            {
+                pol = false;
+                return pol;
+            }
+         }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader input = new StreamReader("input.txt");
+            StreamWriter output = new StreamWriter("output.txt");
+            while (!input.EndOfStream)
+            {
+                string str = input.ReadLine().ToLower();
+                //Console.WriteLine(str);
+                var result = str.BoolPolindrom();
+                output.WriteLine(result);
+            }
+            input.Close();
+            output.Close();
+        }
+    }
+}
